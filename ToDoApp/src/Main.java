bafc3328a20ccc78f252c09bc65a967b35927e92
VVarro <virag.varro@gmail.com>
2017-04-06 14:01:00 +0200

    finish Print usage method

--- ToDoApp/src/Main.java
+++ ToDoApp/src/Main.java
@@ -1,21 +1,28 @@
 public class Main {
   public static void main(String[] args) {
+
     ToDoApp toDoApp = new ToDoApp();
 
     final String DESCR_NAME = "appDescription.txt";
     final String FILE_NAME = "list.csv";
 
     //Kapcsolok
-    if (args[0].equals("-l") && args.length == 1) {
+    if (args.length == 0) {
+      for (int i = 0; i < DESCR_NAME.length(); i++) {
+        System.out.println(toDoApp.readDescriptionFromFile(DESCR_NAME).get(i));
+      }
+    } else if (args[0].equals("-l") && args.length == 1) {
       System.out.println("kiirom a listat");
     } else if (args[0].equals("-a") && args.length == 3) {
       System.out.println("hozzaadok elemet");
+    } else if (args[0].equals("-a") && args.length == 1) {
+      System.out.println("Unable to add: no task provided");
     } else if (args[0].equals("-r") && args.length == 1) {
       System.out.println("torlok egy elemet");
     } else if (args[0].equals("-c") && args.length == 1) {
       System.out.println("megcsinalok egy feladatot");
     } else {
-      System.out.println(toDoApp.readDescriptionFromFile(DESCR_NAME));
+      System.out.println("Try harder!");
     }
 
   }
--- ToDoApp/src/ToDoApp.java
+++ ToDoApp/src/ToDoApp.java
@@ -2,20 +2,45 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ToDoApp {
 
   public ToDoApp() {
   }
 
-  public static String readDescriptionFromFile(String DESCR_NAME) {
+  public static List<String> readDescriptionFromFile(String DESCR_NAME) {
     Path path = Paths.get(DESCR_NAME);
-    String descr = " ";
+    List<String> descr = null;
+
     try {
-      descr = String.valueOf(Files.readAllLines(path));
+      descr = Files.readAllLines(path);
     } catch (IOException e) {
       e.printStackTrace();
     }
     return descr;
+  }
+
+  public static List<String> readListFromFile(String FILE_NAME) {
+    Path path = Paths.get(FILE_NAME);
+    List<String> list = null;
+
+    try {
+      list = Files.readAllLines(path);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return list;
+  }
+
+  private static void processData(List<String> list) {
+    List<String> task = new ArrayList<>();
+    List<String> status = new ArrayList<>();
+    for (int i = 0; i < list.size(); i++) {
+      String[] splittedLine = list.get(i).split(";");
+      task.set(i, splittedLine[0]);
+      status.set(i, splittedLine[1]);
+    }
   }
 }
